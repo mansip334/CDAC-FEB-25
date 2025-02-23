b8dc97c6446d31310389be423d8084a3e1357e65
Mansi Patil <98961123+mansip334@users.noreply.github.com>
2025-02-23 22:35:56 +0530

    Delete Assignment1 directory

--- Assignment1/P1.java
+++
@@ -1,9 +0,0 @@
-// program to print Hello & print separate name
-
-class P1{
-      public static void main(String args[]){
-        
-          System.out.println("Hello");
-          System.out.println("Mansi Patil");
-  }
-}
